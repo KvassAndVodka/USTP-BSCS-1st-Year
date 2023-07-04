aa7f7371e5c7b8a25d4001323bdfdac6a6abca53
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 15:44:23 +0800

    Update and rename Exercise_5.cpp to LN2-E5-FutureInvestmentValue.cpp

--- CS121/ Lectures/Lecture Note 2/LN2-E5-FutureInvestmentValue.cpp
+++ CS121/ Lectures/Lecture Note 2/LN2-E5-FutureInvestmentValue.cpp
@@ -1,10 +1,16 @@
+// Javier M. Raut
+// CS1D - LN2 - E3
+// Future Investment Value
+
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main(){
+    // Declare all Varaibles
     double investmentAmount, futureInvestmentValue, annualInterestRate, numberOfYears, monthlyInterestRate;
 
+    // Ask for user input
     cout << "Enter investment value: ";
     cin >> investmentAmount;
     cout << "Enter annual interest rate in percentage: ";
@@ -12,9 +18,11 @@ int main(){
     cout << "Enter number of years: ";
     cin >> numberOfYears;
 
+    // Calculate for the future investment value
     monthlyInterestRate = annualInterestRate / 1200; 
     futureInvestmentValue = investmentAmount * (pow(1 + monthlyInterestRate, numberOfYears * 12));
 
+    // Display Result
     cout << "Accumulated Value is $" << futureInvestmentValue;
 
     return 0;
