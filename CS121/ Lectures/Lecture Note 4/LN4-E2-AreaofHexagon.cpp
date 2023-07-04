9ffcf65c47f07728de485198a0068c5a5a864918
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 18:48:17 +0800

    Update LN4-E2-AreaofHexagon.cpp
    indentions

--- CS121/ Lectures/Lecture Note 4/LN4-E2-AreaofHexagon.cpp
+++ CS121/ Lectures/Lecture Note 4/LN4-E2-AreaofHexagon.cpp
@@ -9,22 +9,22 @@ using namespace std;
 
 // Declare Function 'AreaOfHexagon'
 double AreaOfHexagon(double s){
-  double Area = (6 * pow(s, 2)) / (4 * tan(M_PI / 6));
-  return Area;
+    double Area = (6 * pow(s, 2)) / (4 * tan(M_PI / 6));
+    return Area;
 }
 
 int main(){
-  // Declare variable s
-  double s;
+    // Declare variable s
+    double s;
 
-  // Ask for user input
-  cout << "Enter the side: ";
-  cin >> s;
+    // Ask for user input
+    cout << "Enter the side: ";
+    cin >> s;
 
-  // Call function AreaOfHexagon and display result
-  double Area = AreaOfHexagon(s);
-  cout << "The are of the Hexagon is " << Area << endl;
+    // Call function AreaOfHexagon and display result
+    double Area = AreaOfHexagon(s);
+    cout << "The are of the Hexagon is " << Area << endl;
 
   
-  return 0; 
+    return 0; 
 }
