18df3f6f41f730babe92185574756e5e57a4bbf4
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 18:25:31 +0800

    Update LN4-E1-GreatCircleDistance.cpp

--- CS121/ Lectures/Lecture Note 4/LN4-E1-GreatCircleDistance.cpp
+++ CS121/ Lectures/Lecture Note 4/LN4-E1-GreatCircleDistance.cpp
@@ -1,16 +1,24 @@
-
+// Javier M. Raut
+// CS1D - LN4 - E1
+// Quadratic Equation Calculator
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
+// Declare values of pi and radius
+const double pi = 3.1415926535;
 const double radius = 6371;
 
+
+// Declare toRadians function
 double toRadians(double degrees) {
-    return degrees * M_PI / 180;
+    return degrees * pi / 180;
 }
 
-double GreatCircleShit(double x1, double y1, double x2, double y2){
+
+// Declare GreatCircle 
+double GreatCircle(double x1, double y1, double x2, double y2){
     double rad_x1, rad_x2, rad_y1, rad_y2; 
     rad_x1 = toRadians(x1);
     rad_x2 = toRadians(x2);
@@ -24,12 +32,16 @@ double GreatCircleShit(double x1, double y1, double x2, double y2){
 
 int main(){
     double x1, y1, x2, y2;
+
+    // Ask user to input points
     cout << "Enter Point 1 (Latitude and Longitude): ";
     cin >> x1 >> y1;
     cout << "Enter Point 2 (Latitude and Longitude): ";
     cin >> x2 >> y2;
 
-    double d = GreatCircleShit(x1, y1, x2, y2);
+
+    // Call Function GreatCircle and display result
+    double d = GreatCircle(x1, y1, x2, y2);
     cout << endl << "The distance between the two points is " << d <<"km.";
 
     return 0;
