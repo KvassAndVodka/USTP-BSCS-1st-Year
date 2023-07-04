20e805a1f42412439a69d9cff170f8dceb664c11
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 15:32:57 +0800

    Update Exercise_1.cpp

--- CS121/ Lectures/Lecture Note 2/Exercise_1.cpp
+++ CS121/ Lectures/Lecture Note 2/Exercise_1.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 
 int main(){
+    //Declare variables pi, radius, and length
     double pi = 3.1415926, radius, length;
 
+    //Ask user for input
     cout << "Enter the radius and length of a cylinder: ";
     cin >> radius >> length; 
-    
+
+    // Solve for area and volume
     double area = radius * radius * pi;
     double volume = area * length;
-    
+
+    //Print out results.
     cout << "The area is " << area << endl; 
     cout << "The volume is " << volume;
     
