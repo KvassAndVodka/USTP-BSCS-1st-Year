7dde121178546e7c1235888a8b2895f80765f253
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 15:42:33 +0800

    Update and rename Exercise_4.cpp to LN2-E4-DrivingCost.cpp

--- CS121/ Lectures/Lecture Note 2/LN2-E4-DrivingCost.cpp
+++ CS121/ Lectures/Lecture Note 2/LN2-E4-DrivingCost.cpp
@@ -1,18 +1,26 @@
+// Javier M. Raut
+// CS1D - LN2 - E4
+// Driving Cost
+
 #include <iostream>
 using namespace std;
 
 int main(){
+    // Declare variables
     double distance, milesPerGalon, pricePerGalon, cost;
 
+    // Ask for input
     cout << "Enter the driving distance: ";
     cin >> distance;
     cout << "Enter miles per gallon: ";
     cin >> milesPerGalon;
     cout << "Enter price per gallon: ";
     cin >> pricePerGalon;
-    
+
+    // Calculate cost
     cost = (distance / milesPerGalon) * pricePerGalon;
 
+    // Display result
     cout << "The cost of driving is: " << cost; 
     
     return 0;
