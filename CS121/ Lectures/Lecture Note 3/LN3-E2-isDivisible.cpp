e2a9f9241bccc7d6969ed3dd46b4e4e4cfcb0315
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 15:52:08 +0800

    Update and rename Exercise_2.cpp to LN3-E2-isDivisible.cpp

--- CS121/ Lectures/Lecture Note 3/LN3-E2-isDivisible.cpp
+++ CS121/ Lectures/Lecture Note 3/LN3-E2-isDivisible.cpp
@@ -1,14 +1,22 @@
+// Javier M. Raut
+// CS1D - LN3 - E2
+// Is Divisible?
+
 #include <iostream>
 using namespace std;
 
 int main(){
+    // Declare variables
     int num1, num2, isDivisible;
 
+    // Ask for user input
     cout << "Enter two integers: ";
     cin >> num1 >> num2;
 
+    // Check if number is divisible
     isDivisible = num1 % num2;
 
+    // Compare and display result.
     if (isDivisible == 0){
         cout << num1 << " is divisible by " << num2;
     } else{
