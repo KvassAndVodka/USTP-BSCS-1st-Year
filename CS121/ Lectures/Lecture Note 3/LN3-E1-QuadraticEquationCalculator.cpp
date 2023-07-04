cee2be9f64ed96f12eea0d0c8413ac438455a50b
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 15:48:52 +0800

    Update LN3-E1-QuadraticEquationCalculator.cpp

--- CS121/ Lectures/Lecture Note 3/LN3-E1-QuadraticEquationCalculator.cpp
+++ CS121/ Lectures/Lecture Note 3/LN3-E1-QuadraticEquationCalculator.cpp
@@ -7,15 +7,19 @@
 using namespace std;
 
 int main(){
+    // Declare Variables
     double a, b, c, discriminant, root1, root2;
 
+    // Ask for user input
     cout << "Enter a, b, c: ";
     cin >> a >> b >> c;
-    
+
+    // Calculate the discriminant and both roots 1 and 2
     discriminant = pow(b, 2) - 4 * a * c;
     root1 = ((-1 * b) + sqrt(discriminant)) / 2 * a;
     root2 = ((-1 * b) - sqrt(discriminant)) / 2 * a;
-    
+
+    // Display Result
     if (discriminant > 0){
         cout << "The roots are " << root1 << " and " << root2;
     } else if (discriminant == 0){
@@ -23,6 +27,6 @@ int main(){
     } else{
         cout << "The equation has no real roots";
     }
-
+    
     return 0; 
 }
