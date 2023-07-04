5038d4f86236d37a5ce7d64c4dcc5d97b1b5f25d
Javier M. Raut <34165073+KvassAndVodka@users.noreply.github.com>
2023-07-04 18:05:08 +0800

    Update and rename Exercise_4.cpp to LN3-E4-RockPaperScissor.cpp

--- CS121/ Lectures/Lecture Note 3/LN3-E4-RockPaperScissor.cpp
+++ CS121/ Lectures/Lecture Note 3/LN3-E4-RockPaperScissor.cpp
@@ -1,19 +1,29 @@
+// Javier M. Raut
+// CS1D - LN3 - E4
+// Rock, Paper, Scissor
 #include <iostream>
 #include <cstdlib>
 
 using namespace std;
 
 int main(){
+    // Declare computer-generated number variable 'gen', and user-inputted variable 'user'
     int gen, user;
+
+    // Ask for user input
     cout << "scissor (0), rock (1), paper (2): ";
     cin >> user;
 
+    // Random number generator for int gen
     srand(time(0));
     gen = rand() % 3;
 
+
+    // Display gen and user
     const string choice[3] = {"scissors", "rock", "paper"};
     cout << "The computer is " << gen[choice] << ". You are " << user[choice];
 
+    // Logic for Winner/Loser
     if(user == gen){
         cout << ". It is a draw.";
     } else if((user == 0 && gen == 2) ||
@@ -23,4 +33,6 @@ int main(){
     } else{
         cout << ". You lose.";
     }
+
+    return 0;
 }
