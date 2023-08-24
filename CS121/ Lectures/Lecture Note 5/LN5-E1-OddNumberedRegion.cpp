955e7dd2e9d0a29cd73e7966af0c2408f0377878
Javier M. Raut <javier.raut@gmail.com>
2023-08-24 23:54:40 +0800

    Improved comments in Exercises 1 and 2

--- CS121/ Lectures/Lecture Note 5/LN5-E1-OddNumberedRegion.cpp
+++ CS121/ Lectures/Lecture Note 5/LN5-E1-OddNumberedRegion.cpp
@@ -7,8 +7,7 @@
 
 using namespace std;
 
-int main(){
-
+int main() {
     //Declare variables for number of trials and Odd region counter
     const int NUMBER_OF_TRIALS = 1000000;
     int OddRegionCount = 0;
@@ -18,21 +17,18 @@ int main(){
     mt19937 gen(rd());
     uniform_real_distribution<> dis(-10, 10);
 
-
     // For loop based on the number of trials
-    for(int i = 0; i < NUMBER_OF_TRIALS; i++){
+    for(int i = 0; i < NUMBER_OF_TRIALS; i++) {
         double x = dis(gen);
         double y = dis(gen);
         double sumOfXY = x + y;
 
         // Checks if rng is inside Region 1 or in region 3
-        if(x < 0 || x > 0 && sumOfXY > 10){
+        if(x < 0 || x >= 0 && y >= 0 && sumOfXY <= 10) {
             OddRegionCount++;
         }
-        
     }
-
-    // Probability calculator
+    // Calculate for probability and display result
     double OddRegion_prob = ((double)OddRegionCount / NUMBER_OF_TRIALS) * 100;
     cout << "The probability of a dart landing in an odd-numbered region is " << OddRegion_prob << "%" << endl;
 
--- CS121/ Lectures/Lecture Note 5/LN5-E2-CommonPrefixDetector.cpp
+++ CS121/ Lectures/Lecture Note 5/LN5-E2-CommonPrefixDetector.cpp
@@ -3,32 +3,36 @@
 // Common Prefix Detector
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
+int main() {
+    // Declare string 1 and 2 and ask for user input
     string str1, str2;
     cout << "Enter s1: ";
     getline(cin, str1);
     cout << "Enter s2: ";
     getline(cin, str2);
 
+    // Measure string length
     int len = min(str1.length(), str2.length());
 
     int i;
-    for (i = 0; i < len; i++){
+    for (i = 0; i < len; i++) {
         if (str1[i] != str2[i])
             break;
     }
 
-    if (i == 0){
-        cout << "No common prefix";
+    // Checks if there is common prefix or not
+    if (i == 0) {
+        cout << str1 << " and " << str2 << " have no common prefix";
     }
     else{
         string prefix = str1.substr(0, i);
         for (int j = i; j > 0; j--) {
             if (str1.substr(0, j) == prefix.substr(0, j)) {
-                cout << "Largest common prefix: " << prefix.substr(0, j) << endl;
+                cout << "The common prefix is " << prefix.substr(0, j) << endl;
                 return 0;
             }
         }
