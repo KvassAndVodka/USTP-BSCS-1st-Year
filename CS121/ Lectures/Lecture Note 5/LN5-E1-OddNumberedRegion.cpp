beb95d0b753d2ca1791b225cdf3e7118873acca0
Javier M. Raut <javier.raut@gmail.com>
2023-08-24 20:48:11 +0800

    Set plane size to 10 and improved region detection and statistics.

--- CS121/ Lectures/Lecture Note 5/LN5-E1-OddNumberedRegion.cpp
+++ CS121/ Lectures/Lecture Note 5/LN5-E1-OddNumberedRegion.cpp
@@ -9,22 +9,25 @@ using namespace std;
 
 int main(){
     const int NUMBER_OF_TRIALS = 1000000;
-    int OddRegion_count = 0;
+    int OddRegionCount = 0;
 
     random_device rd;
     mt19937 gen(rd());
-    uniform_real_distribution<> dis(-1, 1);
+    uniform_real_distribution<> dis(-10, 10);
 
-    for (int i = 0; i < NUMBER_OF_TRIALS; i++){
+    for(int i = 0; i < NUMBER_OF_TRIALS; i++){
         double x = dis(gen);
         double y = dis(gen);
+        double sumOfXY = x + y;
 
-        if (x < 0 || x > 0 && y <=0.5)
-        OddRegion_count++;
+        if(x < 0 || x > 0 && sumOfXY > 10){
+            OddRegionCount++;
+        }
+        
     }
 
-    double OddRegion_prob = ((double)OddRegion_count / NUMBER_OF_TRIALS) * 100;
+    double OddRegion_prob = ((double)OddRegionCount / NUMBER_OF_TRIALS) * 100;
     cout << "The probability of a dart landing in an odd-numbered region is " << OddRegion_prob << "%" << endl;
 
     return 0;
-}   
+}
