063ffd9de18a510a9c8896f3ec09774655ef7943
Javier M. Raut <javier.raut@gmail.com>
2023-08-25 10:05:25 +0800

    Rename and added comments to LN6-E1

--- CS121/ Lectures/Lecture Note 6/LN6-E1-TriangularNumbers.cpp
+++ CS121/ Lectures/Lecture Note 6/LN6-E1-TriangularNumbers.cpp
@@ -1,16 +1,23 @@
+// Javier M. Raut
+// CS1D - L6 - E1 
+// Triangular Numbers
+
 #include <iostream>
 using namespace std;
 
+// Declare function to get triangular number
 int getTriangularNumber(int n) {
     return n * (n + 1) / 2;
 }
 
 int main() {
+    // Declare number of triangular numbers. and line limit
     const int num_of_TriangularNumbers = 75;
     const int line_limit = 5;
 
+    // Get triangular numbers
     int count = 0;
-    for (int i = 1; i <= num_of_TriangularNumbers; i++) {
+    for (int i = 1; i <= num_of_TriangularNumbers; i++)  {
         cout << getTriangularNumber(i) << " ";
         count++;
         if (count == line_limit) {
